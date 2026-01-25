d62bd09d49c2471fd0465aca91672c57f87f55ca
DEVOPEOLUWA <ayodejiope13@gmail.com>
2026-01-25 13:09:01 +0100

    learnt about identifying dsa problem and the right solution analogy to go with

---
+++ arrays/maxProfit.ts
@@ -0,0 +1,11 @@
+function maxProfit(prices: number[]): number {
+    let minPrice = Infinity;
+    let maxProfit = 0;
+    
+    for (let i = 0; i < prices.length; i++) {
+        minPrice = Math.min(minPrice, prices[i]);
+        maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+    }
+    
+    return maxProfit;
+}
